1c56fb3e5216758346e76b1a03b16b6e1fc654bb
Jeremy Moser <Jeremy@qualit.ch>
2023-01-02 22:08:50 +0100

    Added Documentation and TODOs to the Fox class, also updated the searchFood method and added a lot of new variables

--- src/Main/Organisms/Animals/Fox.java
+++ src/Main/Organisms/Animals/Fox.java
@@ -17,86 +17,137 @@ public class Fox extends Animal {
             "sepWeight", "aliWeight", "cohWeight"
             }
     );
-    public static int totalAmountOfFoxes = 0;
+    public static int totalAmountOfFoxes = 0;                   //total amount of Foxes naturally born
+
+    public static final int MAX_HEALTH = 1000;                  //maximum health for all Foxes
+    public static final int STARTING_HEALTH = 500;              //starting health of a Fox
+    public static final int MAX_HUNTING_HEALTH = 800;           //above this threshold the animal will stop hunting food
+    public static final double[][] HEALTH_REPRODUCTION_BONUS = new double[][]{
+            new double[]{MAX_HEALTH*.75, MAX_HEALTH*.5, MAX_HEALTH*.25},    //health threshold at which there is a reproduction bonus
+            new double[]{.002,.001,.0005}                                   //bonus to reproduction
+    };
+
+    public static final double BASE_REPRODUCTION_CHANCE = 0;    //Base reproduction chance
+    public static final double MUTATION_CHANCE = .5;            //Chance for mutation of a Gene
+
+    public static final double DAMAGE = 300;                    //damage an attack of a Fox does
+
+    public static final double BASE_SIZE = 7;                   //Base size of a Fox
+    public Color col = new Color(237, 150, 11, 200);
 
     //Constructors
     public Fox(Transform transform, float health, DNA dna){
         super(transform, health, dna);
-        this.decodeDNA();
-        //start with a random velocity
-        this.transform.velocity = Vector2D.randLimVec((Math.random()-.5)*5,(Math.random()-.5)*5);
-        //add this DNA to the collection
-        sumDNA.addToAVG(this.sumDNA,totalAmountOfFoxes, this.dna);
-        totalAmountOfFoxes++;
+        this.decodeDNA();                                           //initialize DNA
+
+        this.transform.velocity = Vector2D.randLimVec((Math.random()-.5)*5,
+                (Math.random()-.5)*5);                        //start with a random velocity
+
+        sumDNA.addToAVG(this.sumDNA,totalAmountOfFoxes, this.dna);  //add this DNA to the collection
+        totalAmountOfFoxes++;                                       //increase counter
     }
     public Fox(){
         super();
         this.dna = new DNA(11);
-        this.decodeDNA();
-        this.health = 600;
-        //start with a random velocity
-        this.transform.velocity = Vector2D.randLimVec((Math.random()-.5)*5,(Math.random()-.5)*5);
+        this.decodeDNA();                                           //initialize DNA
+
+        this.health = STARTING_HEALTH;
+        this.transform.velocity = Vector2D.randLimVec((Math.random()-.5)*5,
+                (Math.random()-.5)*5);                       //start with a random velocity
     }
 
+    /**
+     * This method checks if a Fox collides with his Prey and if so deal damage and gain health if the prey is dead
+     * PRE-CONDITION: the Prey needs to be of the correct class, this mustn't be dead
+     * @param Prey the target that should be checked for a collision with
+     * @return true if the prey is dead
+     */
     @Override
-    public boolean collision(Organism Rabbit) {
-        assert Rabbit.getClass() == Rabbit.class;
-        if(this.transform.location.dist(Rabbit.transform.location) <= this.transform.getR() + Rabbit.transform.getR()){
-            if(this.health >= 600) this.health = 600;
-            Rabbit.health -= 500;                                     //reduce plants health to 0
-            if(Rabbit.health <= 0) this.health += Rabbit.transform.size * 100;
-            //System.out.println(this.health);
-            target = null;                                  //remove target
-            //System.out.println("eating");
-            assert this.transform.velocity.magSq() <= this.maxSpeed*this.maxSpeed;
-            return true;
+    public boolean collision(Organism Prey) {
+        //TODO: create a Prey variable that holds the class of all huntable / eatable / fightable organisms
+        assert this.dead() : "This is dead";                                            //check if this is dead
+        assert Prey.getClass() == Rabbit.class;                                         //check if the target is a Rabbit
+
+        //check if the two collide
+        if(this.transform.location.dist(Prey.transform.location) <= this.transform.getR() + Prey.transform.getR()){
+            //TODO: maybe make this dependant on attributes of the Fox (size, ect)
+            Prey.health -= DAMAGE;                                                      //reduce Preys health by dmg
+            //TODO: rework so that once an animal is dead it leaves a corpse that then can be consumed
+            //TODO: make damaged animals walk slower for a short period of time
+            if(Prey.health <= 0) {                                                      //if the prey is dead
+                this.health += Prey.transform.size * 100;                               //adds health if the rabbit is dead
+                target = null;                                                          //remove target
+            }
         }
-        assert this.transform.velocity.magSq() <= this.maxSpeed*this.maxSpeed;
-        return false;
+        assert this.invariant(): "Invariant is broken";
+
+        return target == null;                                                          //return true if the prey has been killed
     }
 
+    /**
+     * Searches for possible Prey if the Fox is hungry enough and doesn't already have a target
+     * PRE-CONDITION: this mustn't be dead
+     * @param organisms that can be hunted
+     * @return the Prey
+     */
     //TODO: rework for better performance
+    //TODO: consider health of an animal too, if a Prey is lower health it should get prioritized
     @Override
     public Organism searchFood(ArrayList<Organism> organisms) {
+        assert this.dead() : "This is dead";
+
         Organism closestFood = null;
-        if(target != null && this.health <= 500) {
-            this.transform.applyForce(seek(target.getLocation()));
-            if (collision(target)) target = searchFood(organisms);
-        }else{
+
+        //if there is a target and the Fox is hungry enough to look for food
+        if(target != null && this.health <= MAX_HUNTING_HEALTH) {
+            this.transform.applyForce(seek(target.getLocation()));          //seek the target
+            if (collision(target)) target = searchFood(organisms);          //if the target is dead
+        }
+        //else look for food
+        else{
             double closestDistance = Double.POSITIVE_INFINITY;
             for(Organism o : organisms){
-                assert o.getClass() == Rabbit.class;
-                double distance = Vector2D.sub(o.transform.location, this.transform.location).mag();
-                if((closestDistance >= distance) && (distance <= this.viewDistance)){
+                //TODO: create a Prey variable that holds the class of all huntable / eatable / fightable organisms
+                assert o.getClass() == Rabbit.class;                                //check if the target is a Rabbit
+
+                double distance = Vector2D.sub(o.transform.location, this.transform.location).magSq();
+                //if the distance is smaller than the current closest distance and smaller than the viewDistance
+                if((closestDistance >= distance) && (distance <= Math.pow(this.viewDistance,2))){
                     closestDistance = distance;
                     closestFood = o;
                 }
             }
-            if(closestFood != null) assert closestFood.getClass() == Rabbit.class;
+            if(closestFood != null) assert closestFood.getClass() == Rabbit.class;  //check if the target is a Rabbit
             this.target = closestFood;
         }
-        assert this.transform.velocity.magSq() <= this.maxSpeed*this.maxSpeed;
+        assert this.invariant(): "Invariant is broken";
         return closestFood;
     }
 
+    /**
+     * Calculates the Chance to give birth and adds boni according to this Foxes health
+     * Then gives birth if the Chance occurs and creates a new Fox
+     * PRE-CONDITION: this mustn't be dead
+     */
     @Override
     public void reproduce(){
-        double birthChance = 0;
-        if(this.health >= 500){
-            birthChance = .01;
-        }else if(this.health >= 400){
-            birthChance = .005;
-        }else if(this.health >= 300){
-            birthChance = .0025;
+        assert this.dead() : "This is dead";
+
+        double birthChance = BASE_REPRODUCTION_CHANCE;
+        //Adds all Boni that for each threshold that has been met
+        for(int i = 0; i < HEALTH_REPRODUCTION_BONUS[0].length; i++){
+            if(this.health >= HEALTH_REPRODUCTION_BONUS[0][i]) birthChance += HEALTH_REPRODUCTION_BONUS[1][i];
         }
         if(Math.random() <= birthChance){
-            DNA childDNA = dna.copy();
-            childDNA.mutate(.5);
-            Transform t = this.transform.clone();
-            CFrame.Foxes.add(new Fox(t,600, childDNA));
+            DNA childDNA = dna.copy();                              //copy this DNA
+            childDNA.mutate(MUTATION_CHANCE);                       //mutate DNA if MUTATION_CHANCE occurs
+            Transform t = this.transform.clone();                   //Copy this transform
+            CFrame.Foxes.add(new Fox(t,MAX_HEALTH, childDNA));      //Add a new Fox
         }
+        assert this.invariant(): "Invariant is broken";
     }
 
+    //right now there aren't any animals that a fox needs to flee of so this is currently empty
     @Override
     public void flee(ArrayList<Organism> organisms) {
     }
@@ -113,24 +164,22 @@ public class Fox extends Animal {
      * genes[8] = separationWeight
      * genes[9] = alignmentWeight
      * genes[10] = cohesionWeight
+     *
+     * Decodes the DNA and sets the attributes according to it
      */
+    //TODO: check what happens when values here are negative
     @Override
     public void decodeDNA() {
-        //Define Main.Main.NeuralNetwork.NeuralNetwork.Gender
-        /*
-        if(this.dna.genes[0]) this.gender = Main.Main.NeuralNetwork.NeuralNetwork.Gender.MALE;
-        else this.gender = Main.Main.NeuralNetwork.NeuralNetwork.Gender.FEMALE;
-         */
         if(Math.random() <= .5) this.gender = Gender.MALE;
         else this.gender = Gender.FEMALE;
         //Define size
-        this.transform.size = this.dna.genes[1]+7;
+        this.transform.size = this.dna.genes[1]+BASE_SIZE;
         //Define maxSpeed
         this.maxSpeed = this.dna.genes[2];
         //Define maxForce
         this.maxForce = this.dna.genes[3];
         //Define viewDistance
-        this.viewDistance = this.dna.genes[4]+this.transform.size;
+        this.viewDistance = this.dna.genes[4] * this.transform.size;
         //Define separation, alignment, cohesion distances
         this.desiredSepDist = this.dna.genes[5] * this.transform.size;
         this.desiredAliDist = this.dna.genes[6] * this.transform.size;
@@ -139,7 +188,8 @@ public class Fox extends Animal {
         this.sepWeight = this.dna.genes[8];
         this.aliWeight = this.dna.genes[9];
         this.cohWeight = this.dna.genes[10];
-        //System.out.println(this.dna);
+
+        assert this.invariant(): "Invariant is broken";
     }
 
     //Behavior
@@ -157,9 +207,8 @@ public class Fox extends Animal {
             //this.health -= Vector2D.map(this.transform.velocity.mag(),0,this.maxSpeed,0, 3);
         //}else
             this.health-=5;
-        assert this.transform.velocity.magSq() <= this.maxSpeed*this.maxSpeed;
 
-        //System.out.println(this.health);
+        assert this.invariant(): "Invariant is broken";
     }
 
     @Override
@@ -167,12 +216,21 @@ public class Fox extends Animal {
 
     }
 
+    /**
+     * Corrects if an Animal has too much health
+     * @return if the current speed is bigger than the maxSpeed
+     */
+    public boolean invariant(){
+        if(this.health >= MAX_HEALTH) this.health = MAX_HEALTH;
+        return this.transform.velocity.magSq() <= this.maxSpeed*this.maxSpeed;
+    }
+
     //Visualization
     @Override
+    //TODO: make each animal have a variable Color
     public void paint(Graphics2D g) {
-        Color c = new Color(237, 150, 11, 200);
-        g.setColor(c);
-        //g.draw(this.transform.getRectangle());
+        g.setColor(this.col);
         g.fill(this.transform.getRectangle());
+        assert this.invariant(): "Invariant is broken";
     }
 }
