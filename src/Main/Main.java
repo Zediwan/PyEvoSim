8a0fcc494d6954dc7371d18f41a3e18bce5892ff
Jeremy Moser <Jeremy@qualit.ch>
2023-02-21 22:54:19 +0100

    Created the Simulation and World classes but still have a lot of problems, maybe need to tear the whole thing down and restart with one single animal

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,10 +7,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        JPanel simulationPanel = new JPanel();
-        simulationPanel.setBackground(Color.lightGray);
-        simulationPanel.setPreferredSize( new Dimension(1200,600));
-
+        /*
         JPanel statPanel = new JPanel();
         statPanel.setBackground(Color.white);
         statPanel.setPreferredSize(new Dimension(200,200));
@@ -18,8 +15,18 @@ public class Main
         JPanel graphPanel = new JPanel();
         graphPanel.setBackground(Color.darkGray);
         graphPanel.setPreferredSize(new Dimension(200,200));
-
-        JFrame frame = new JFrame("Simulation");
+         */
+        //CFrame frame2 = new CFrame();
+        World w = new World(400,400,10,10);
+        Simulation s = new Simulation(100,100,100,
+                100,100,100,
+                100,100,100,
+                w);
+        s.initiatePopulation();
+        //JPanel simulationPanel = s.setUpVisuals();
+        //simulationPanel.setBackground(Color.lightGray);
+        /*
+         JFrame frame = new JFrame("Simulation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,1000);
         frame.setLayout(new BorderLayout(0,0));
@@ -27,13 +34,8 @@ public class Main
         //frame.add(new CFrame());
         frame.add(graphPanel, BorderLayout.SOUTH);
         frame.add(statPanel, BorderLayout.EAST);
-        frame.add(simulationPanel, BorderLayout.CENTER);
+        //frame.add(simulationPanel, BorderLayout.CENTER);
         frame.setVisible(true);
-
-
-        CFrame frame2 = new CFrame();
-        //CFrame frame1 = new CFrame(3200, 0 ,0);
-        //CFrame frame2 = new CFrame(3200, 0, 2);
-        //CFrame frame3 = new CFrame(1600, 2, 2);
+         */
     }
 }
--- src/Main/World.java
+++ src/Main/World.java
@@ -1,10 +1,43 @@
 package Main;
 
-public class World {
-    static final int TIME_PERIOD = 24;  //TODO: find out what this is for
-    static int time = 0;
+import Main.Organisms.Organism;
 
-    //FRAME SIZES
-    public static final int WIDTH = 800; //width of the frame
-    public static final int HEIGHT = 800; //height of the frame
+import java.awt.*;
+
+public class World {
+    private Dimension worldDimension;     //world size
+    private Grid grid;
+
+    public World(int width, int height, int gridFieldWidth, int gridFieldHeight){
+        this.worldDimension = new Dimension(width, height);
+        this.grid = new Grid(gridFieldWidth, gridFieldHeight, this); //initiate the grid
+    }
+
+    public void updatePlant(Organism o) {
+        this.grid.updateGridP(o);
+    }
+
+    public void updateRabbit(Organism o) {
+        this.grid.updateGridR(o);
+    }
+
+    public void updateFox(Organism o) {
+        this.grid.updateGridF(o);
+    }
+
+    public Dimension getWorldDimension() {
+        return worldDimension;
+    }
+
+    public void setWorldDimension(Dimension worldDimension) {
+        this.worldDimension = worldDimension;
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
 }
