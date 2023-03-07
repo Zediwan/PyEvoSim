ce8ba6498590d998ccde667dff15041d7607bb4a
Jeremy Moser <Jeremy@qualit.ch>
2023-03-07 22:21:09 +0100

    Reworked the whole DNA now using a dictionary, still needs testing

--- src/Main/Organisms/Attributes/DNA.java
+++ src/Main/Organisms/Attributes/DNA.java
@@ -1,16 +1,70 @@
 package Main.Organisms.Attributes;
 
-import Main.Helper.Vector2D;
-
 import java.awt.*;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DNA {
-    Random rand = new Random();
+    private Map<String, Double> phenotypeDict;
+
+    public DNA() {
+        this.phenotypeDict = new HashMap<>();
+    }
+
+    public void addPhenotypeFactor(String name, double value) {
+        phenotypeDict.put(name, value);
+    }
+
+    public Object getPhenotypeFactor(String name) {
+        return phenotypeDict.get(name);
+    }
+
+    public void setPhenotypeFactor(String name, double value) {
+        phenotypeDict.put(name, value);
+    }
+
+    public void mutate(double mutChance, double mutRange) {
+        for (String name : phenotypeDict.keySet()) {
+            double newValue = phenotypeDict.get(name);
+            if(Math.random() < mutChance){
+                newValue += Math.random()*mutRange - (mutRange/2.0);
+            }
+            phenotypeDict.put(name, newValue);
+        }
+    }
+
+    public DNA copy() {
+        DNA newDNA = new DNA();
+        for (String name : phenotypeDict.keySet()) {
+            double copyValue = phenotypeDict.get(name);
+            newDNA.setPhenotypeFactor(name, copyValue);
+        }
+        return newDNA;
+    }
+
+    /*
+    private Object mutateValue(Object value) {
+        if (value instanceof Integer) {
+            int intValue = (int)value;
+            int bitIndex = (int)(Math.random() * 32);
+            intValue ^= 1 << bitIndex;
+            return intValue;
+        } else if (value instanceof Double) {
+            double doubleValue = (double)value;
+            doubleValue += Math.random() * 0.1 - 0.05;
+            return doubleValue;
+        } else if (value instanceof Boolean) {
+            boolean booleanValue = (boolean)value;
+            return !booleanValue;
+        }
+        // Add more mutation logic for other types as needed
+        return value;
+    }
+     */
+    //LEGACY CODE
+    /*
     public double[] genes;
     public String[] names;
-
     public DNA(int num, double range){
         this.genes = new double[num];
         for(int i = 0; i < this.genes.length; i++) this.genes[i] = this.rand.nextDouble(range)-(range/2);
@@ -30,8 +84,6 @@ public class DNA {
         this.genes = genes;
         this.names = names;
     }
-
-
     public DNA copy(){
         double[] newGenes = new double[this.genes.length];
         System.arraycopy(this.genes, 0, newGenes, 0, this.genes.length);    //copy all elements
@@ -78,17 +130,6 @@ public class DNA {
         for(int i = 0; i < dna.genes.length; i++) resultDNA.genes[i] = dna.genes[i] / num ;
         return resultDNA;
     }
-
-    public String toString(){
-        String s = "[ ";
-        for (double gene : this.genes) {
-            if (gene >= 0) s += "+";
-            s += String.format("%.2f", gene) + ", ";
-        }
-        s += "]";
-        return s;
-    }
-
     public void paint(Graphics2D g, int x, int y) {
         //TODO: figure out how long the longest string is and auto-scale the positions according to it
         for(int i = 0; i < this.genes.length; i++){
@@ -108,5 +149,36 @@ public class DNA {
 
     public void paint(Graphics2D g, Vector2D loc) {
         this.paint(g, (int)loc.x,(int)loc.y);
+    }
+     */
+
+    public String toString(){
+        String s = "[ ";
+        for (String name : phenotypeDict.keySet()) {
+            if (phenotypeDict.get(name) >= 0) s += "+";
+            s += String.format("%.2f", name) + ", ";
+        }
+        s += "]";
+        return s;
+    }
+
+    public void paint(Graphics2D g, int x, int y) {
+        int rowHeight = 20;
+        int cellWidth = 100;
+        int tableWidth = 2 * cellWidth;
+        int tableHeight = (phenotypeDict.size() + 1) * rowHeight;
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, tableWidth, tableHeight);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, tableWidth, tableHeight);
+
+        int row = 1;
+        for (String name : phenotypeDict.keySet()) {
+            Object value = phenotypeDict.get(name);
+            String valueString = value.toString();
+            g.drawString(name, x + 10, y + rowHeight * row);
+            g.drawString(valueString, x + cellWidth + 10, y + rowHeight * row);
+            row++;
+        }
     }
 }
---
+++ src/Main/Organisms/Attributes/Plant_DNA.java
@@ -0,0 +1,4 @@
+package Main.Organisms.Attributes;
+
+public class Plant_DNA extends DNA{
+}
