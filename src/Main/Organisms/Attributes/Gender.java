f4ec16dcd719e6f9b1e3e67f761a785ed9e96f16
Jeremy Moser <Jeremy@qualit.ch>
2023-03-07 22:53:06 +0100

    Implemented the Diet enum

--- src/Main/Organisms/Attributes/Gender.java
+++ src/Main/Organisms/Attributes/Gender.java
@@ -1,17 +1,15 @@
 package Main.Organisms.Attributes;
 
-import Main.CFrame;
-
 public enum Gender {
     //TODO: implement methods that come with being female f.e. getPregnant and giveBirth
     MALE,
     FEMALE;
 
     /**
-     * @return a random Main.Main.NeuralNetwork.NeuralNetwork.Gender
+     * @return a random Gender
      */
     public static Gender getRandomGender(){
-        if(CFrame.random.nextBoolean())return Gender.MALE;
+        if(Math.random() < .5)return Gender.MALE;
         else return Gender.FEMALE;
     }
 }
