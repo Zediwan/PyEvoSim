ac9398cd0c9f5729a2f10ccf2d9ce5570bc56204
Jeremy Moser <Jeremy@qualit.ch>
2023-03-08 17:20:11 +0100

    Implemented the mutable interface

---
+++ src/Main/Helper/mutable.java
@@ -0,0 +1,7 @@
+package Main.Helper;
+
+public interface mutable {
+    //TODO: check if this interface is applied to all necessary classes
+    public void mutate();
+    public void mutate(double mutationChance, double range);
+}
