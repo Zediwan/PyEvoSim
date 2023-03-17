36cdf67f958d89adb17f5881f3860b8e06a878bc
Jeremy Moser <Jeremy@qualit.ch>
2023-03-17 17:27:00 +0100

    Matrices are not mutable

--- src/Main/Helper/Matrix.java
+++ src/Main/Helper/Matrix.java
@@ -5,25 +5,25 @@ import java.util.function.Function;
 public class Matrix implements mutable {
     //TODO: write Test cases
     //TODO: couldn't we remove the rows and cols indicator and jsut use .length()
-    private final int ROWS;
-    private final int COLS;
+    private final int rows;
+    private final int cols;
     public double[][] data;
 
     public Matrix(int rows, int cols){
-        this.ROWS = rows;
-        this.COLS = cols;
+        this.rows = rows;
+        this.cols = cols;
         this.data = new double[rows][cols];
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] = 0;
             }
         }
     }
 
     public Matrix copy() {
-        Matrix m = new Matrix(this.ROWS, this.COLS);
-        for (int i = 0; i < this.ROWS; i++) {
-            for (int j = 0; j < this.COLS; j++) {
+        Matrix m = new Matrix(this.rows, this.cols);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.cols; j++) {
                 m.data[i][j] = this.data[i][j];
             }
         }
@@ -39,9 +39,9 @@ public class Matrix implements mutable {
     }
 
     public double[] toArray(){
-        double[] arr = new double[this.ROWS +this.COLS];
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        double[] arr = new double[this.rows+this.cols];
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 arr[r+c] = this.data[r][c];
             }
         }
@@ -50,26 +50,24 @@ public class Matrix implements mutable {
 
 
     public Matrix randomize(){
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] = Math.random()-.5;
             }
         }
         return this;
     }
-
     public Matrix randomize(double range){
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] += (Math.random()*range)-(range/2);
             }
         }
         return this;
     }
-
     public Matrix randomize(double range, double chance){
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 if(Math.random()< chance){
                     this.data[r][c] += (Math.random()*range)-(range/2);
                 }
@@ -80,8 +78,8 @@ public class Matrix implements mutable {
     public static Matrix randomize(Matrix m1){
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 m1.data[r][c] = Math.random()-.5;
             }
         }
@@ -90,8 +88,8 @@ public class Matrix implements mutable {
 
 
     public Matrix add(double scalar){
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] += scalar;
             }
         }
@@ -100,9 +98,9 @@ public class Matrix implements mutable {
     public static Matrix add(Matrix m1, double scalar){
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[r][c] = m1.data[r][c] + scalar;
             }
         }
@@ -110,12 +108,12 @@ public class Matrix implements mutable {
     }
 
     public Matrix add(Matrix oM){
-        assert this.COLS == oM.COLS : "Cols don't match: " + this.COLS + "!=" + oM.COLS;
-        assert this.ROWS == oM.ROWS : "Rows don't match: " + this.ROWS + "!=" + oM.ROWS;;
+        assert this.cols == oM.cols : "Cols don't match: " + this.cols + "!=" + oM.cols;
+        assert this.rows == oM.rows : "Rows don't match: " + this.rows + "!=" + oM.rows;;
         assert oM.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] += oM.data[r][c];
             }
         }
@@ -125,9 +123,9 @@ public class Matrix implements mutable {
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
         assert m2.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[r][c] = m1.data[r][c] + m2.data[r][c];
             }
         }
@@ -135,12 +133,12 @@ public class Matrix implements mutable {
     }
 
     public Matrix sub(Matrix oM){
-        assert this.COLS == oM.COLS : "Cols don't match: " + this.COLS + "!=" + oM.COLS;
-        assert this.ROWS == oM.ROWS : "Rows don't match: " + this.ROWS + "!=" + oM.ROWS;;
+        assert this.cols == oM.cols : "Cols don't match: " + this.cols + "!=" + oM.cols;
+        assert this.rows == oM.rows : "Rows don't match: " + this.rows + "!=" + oM.rows;;
         assert oM.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] -= oM.data[r][c];
             }
         }
@@ -150,9 +148,9 @@ public class Matrix implements mutable {
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
         assert m2.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[r][c] = m1.data[r][c] - m2.data[r][c];
             }
         }
@@ -161,8 +159,8 @@ public class Matrix implements mutable {
 
 
     public Matrix mult(double scalar){
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] *= scalar;
             }
         }
@@ -171,9 +169,9 @@ public class Matrix implements mutable {
     public static Matrix mult(Matrix m1, double scalar){
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[r][c] = m1.data[r][c] * scalar;
             }
         }
@@ -181,15 +179,15 @@ public class Matrix implements mutable {
     }
 
     public static Matrix mult(Matrix m1, Matrix m2){
-        assert m1.COLS == m2.ROWS : "Cols and Rows don't match; " + m1.COLS + "!=" + m2.ROWS;
+        assert m1.cols == m2.rows : "Cols and Rows don't match; " + m1.cols + "!=" + m2.rows;
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
         assert m2.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.ROWS, m2.COLS);
-        for(int r = 0; r < result.ROWS; r++){
-            for(int c = 0; c < result.COLS; c++){
+        Matrix result = new Matrix(m1.rows, m2.cols);
+        for(int r = 0 ; r < result.rows ; r++){
+            for(int c = 0 ; c < result.cols ; c++){
                 double sum = 0;
-                for(int k = 0; k < m1.COLS; k++){
+                for(int k = 0 ; k < m1.cols ; k++){
                     sum += m1.data[r][k] * m2.data[k][c];
                 }
                 result.data[r][c] = sum;
@@ -202,21 +200,21 @@ public class Matrix implements mutable {
     public Matrix mult(Matrix oM){
         assert oM.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 this.data[r][c] *= oM.data[r][c];
             }
         }
         return this;
     }
     public static Matrix elemMult(Matrix m1, Matrix m2){
-        assert m1.ROWS != m2.ROWS || m1.COLS != m2.COLS : "Columns and Rows of A must match Columns and Rows of B.";
+        assert m1.rows != m2.rows || m1.cols != m2.cols : "Columns and Rows of A must match Columns and Rows of B.";
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
         assert m2.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[r][c] = m1.data[r][c] * m2.data[c][r];
             }
         }
@@ -227,9 +225,9 @@ public class Matrix implements mutable {
     public static Matrix transpose(Matrix m1) {
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.COLS, m1.ROWS);
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
+        Matrix result = new Matrix(m1.cols, m1.rows);
+        for(int r = 0 ; r < m1.rows; r++){
+            for(int c = 0; c < m1.cols; c++){
                 result.data[c][r] = m1.data[r][c];
             }
         }
@@ -243,35 +241,15 @@ public class Matrix implements mutable {
 
     @Override
     public void mutate(double mutationChance, double range) {
-        Matrix mutation = new Matrix(this.ROWS,this.COLS).randomize(mutationChance,range);
+        Matrix mutation = new Matrix(this.rows,this.cols).randomize(mutationChance,range);
         this.add(mutation);
-    }
-
-    //TODO should this only be done with same size matrices? or how would different sized matrices be handled?
-    public static Matrix crossover(Matrix m1, Matrix m2){
-        assert(m1.ROWS == m2.ROWS) : "Different amount of Rows";
-        assert(m1.COLS == m2.COLS) : "Different amount of Cols";
-
-        Matrix m3 = m1.copy();
-        for(int r = 0; r < m1.ROWS; r++){
-            for(int c = 0; c < m1.COLS; c++){
-                /*
-                Select randomly one of the two matrices values
-                Here as we start with m1's values we only need to choose m2's values 50% of the time
-                 */
-                if(Math.random() < .5) {
-                    m3.data[r][c] = m2.data[r][c];
-                }
-            }
-        }
-        return m3;
     }
 
     public Matrix map(Function<Double, Double> f){
         assert this.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        for(int r = 0; r < this.ROWS; r++){
-            for(int c = 0; c < this.COLS; c++){
+        for(int r = 0 ; r < this.rows; r++){
+            for(int c = 0; c < this.cols; c++){
                 double val = this.data[r][c];
                 this.data[r][c] = f.apply(val);
             }
@@ -281,9 +259,9 @@ public class Matrix implements mutable {
     public static Matrix map(Matrix m1, Function<Double, Double> f) {
         assert m1.data != null : "Main.Main.NeuralNetwork.NeuralNetwork.Matrix is null";
 
-        Matrix result = new Matrix(m1.ROWS, m1.COLS);
-        for(int r = 0; r < result.ROWS; r++){
-            for(int c = 0; c < result.COLS; c++){
+        Matrix result = new Matrix(m1.rows, m1.cols);
+        for(int r = 0 ; r < result.rows; r++){
+            for(int c = 0; c < result.cols; c++){
                 double val = m1.data[r][c];
                 result.data[r][c] = f.apply(val);
             }
@@ -291,27 +269,11 @@ public class Matrix implements mutable {
         return result;
     }
 
-    public int getROWS() {
-        return ROWS;
-    }
-
-    public int getCOLS() {
-        return COLS;
-    }
-
-    public double[][] getData() {
-        return data;
-    }
-
-    public void setData(double[][] data) {
-        this.data = data;
-    }
-
     public String toString(){
         String s = "";
-        for(int r = 0; r < this.ROWS; r++){
+        for(int r = 0 ; r < this.rows; r++){
             s+= "| ";
-            for(int c = 0; c < this.COLS; c++){
+            for(int c = 0; c < this.cols; c++){
                 s += data[r][c];
                 s += " ";
             }
