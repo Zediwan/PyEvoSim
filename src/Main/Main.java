a86447579a176bd3e1fd0dc29794458f9be1b3fe
Jeremy Moser <Jeremy@qualit.ch>
2023-03-23 14:01:31 +0100

    Updated the main class to fit the new simulation constructor

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,10 +5,14 @@ public class Main
     public static void main(String[] args)
     {
         World w = new World(400,400,10,10);
-        Simulation s = new Simulation(100,100,100,
-                100,100,100,
-                100,100,100,
+        Simulation s = new Simulation(10000,100,100000,
+                80000,100000,10,
+                100,100,
                 w);
         s.initiatePopulation();
+        while(true){
+            //s.simFrame.getGraphics().fillOval(500,500,10,10);
+            s.paint(s.simFrame.getGraphics());
+        }
     }
 }
