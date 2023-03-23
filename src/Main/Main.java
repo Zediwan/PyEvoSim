83faaa52037444e6231782a1fcd4f38107303eac
Jeremy Moser <Jeremy@qualit.ch>
2023-03-23 14:17:38 +0100

    Made sim window larger

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        World w = new World(400,400,10,10);
+        World w = new World(1000,800,10,10);
         Simulation s = new Simulation(10000,100,100000,
                 80000,100000,10,
                 100,100,
