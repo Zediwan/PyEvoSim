ad29601a844ef96bf010b2f0fae30c519af3e2c3
Jeremy Moser <Jeremy@qualit.ch>
2023-03-25 10:04:40 +0100

    Created a class for the animal testing, noting done yet

---
+++ src/Test/AnimalTest.java
@@ -0,0 +1,98 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AnimalTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void emptyConstructor(){
+
+    }
+
+    @Test
+    void blueprint() {
+    }
+
+    @Test
+    void expressGenes() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void grow() {
+    }
+
+    @Test
+    void think() {
+    }
+
+    @Test
+    void mate() {
+    }
+
+    @Test
+    void reproduce() {
+    }
+
+    @Test
+    void collision() {
+    }
+
+    @Test
+    void searchClosestPlant() {
+    }
+
+    @Test
+    void searchClosestAnimal() {
+    }
+
+    @Test
+    void searchClosestMate() {
+    }
+
+    @Test
+    void searchClosest() {
+    }
+
+    @Test
+    void metabolismCost() {
+    }
+
+    @Test
+    void growthRate() {
+    }
+
+    @Test
+    void healthRatio() {
+    }
+
+    @Test
+    void seek() {
+    }
+
+    @Test
+    void separate() {
+    }
+
+    @Test
+    void cohesion() {
+    }
+
+    @Test
+    void align() {
+    }
+}
