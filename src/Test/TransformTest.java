c497ec5381cf3ec9430c2e1e84444db645bdc135
Jeremy Moser <Jeremy@qualit.ch>
2023-04-13 22:41:55 +0200

    Wrote Tests for Transform.angleBetween()

---
+++ src/Test/TransformTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TransformTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getRotation() {
+
+    }
+}
