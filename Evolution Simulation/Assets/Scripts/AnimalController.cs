a83f54ca8a77f94e2e0a62d13b720aca7d00d82a
Zediwan <Jeremy@qualit.ch>
2023-06-14 12:38:19 +0200

    Readded the deleted scripts

--- Evolution Simulation/Assets/Scripts/AnimalController.cs
+++ Evolution Simulation/Assets/Scripts/AnimalController.cs
@@ -5,31 +5,71 @@ using UnityEngine;
 //https://www.youtube.com/playlist?list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4
 public class AnimalController : MonoBehaviour
 {
+    public NeatNetwork myNetwork;
+    public int inputNodes = 3, outputNodes = 2, hiddenNodes; // Number of respective nodes for the initial network
+    private float[] sensors;
+    private float hitDivider = 20f;
+    private float rayDistance = 40f;
+
     [Range(-1f,1f)]
     public float a,t;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        sensors = new float[inputNodes];    //Initialize the sensors
+        myNetwork = new NeatNetwork(inputNodes, outputNodes, hiddenNodes);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        MoveAnimal(a,t);
+        InputSensors();
+        float[] outputs = myNetwork.FeedForwardNetwork(sensors); // Pass in sensor information
+        MoveAnimal(outputs[0], outputs[1]);
+    }
+
+    private void InputSensors()
+    {
+        Ray r = new Ray(transform.position, transform.up);
+        RaycastHit hit;
+        if(Physics.Raycast(r, out hit, rayDistance))
+        {
+            if(hit.transform.tag == "Wall")
+            {
+                sensors[0] = hit.distance / hitDivider;
+                Debug.DrawLine(r.origin, hit.point, Color.white);
+            }
+        }
+        r.direction = (transform.up + transform.right);
+        if(Physics.Raycast(r, out hit, rayDistance))
+        {
+            if(hit.transform.tag == "Wall")
+            {
+                sensors[0] = hit.distance / hitDivider;
+                Debug.DrawLine(r.origin, hit.point, Color.white);
+            }
+        }
+        r.direction = (transform.up - transform.right);
+        if(Physics.Raycast(r, out hit, rayDistance))
+        {
+            if(hit.transform.tag == "Wall")
+            {
+                sensors[0] = hit.distance / hitDivider;
+                Debug.DrawLine(r.origin, hit.point, Color.white);
+            }
+        }
     }
 
     public void MoveAnimal(float v, float h)
     {
-        // Getting Next Position
+        // Getting Next Position.
         Vector3 input = Vector3.Lerp(Vector3.zero, new Vector3(0, v * 2f, 0), 0.1f);
         input = transform.TransformDirection(input);
 
-        // Actual Movement of Agent
+        // Actual Movement of Agent.
         transform.position += input;
 
-        // Rotation of Agent
+        // Rotation of Agent.
         transform.eulerAngles += new Vector3(0, 0, (h*90)*0.1f);
     }
 }
--- Evolution Simulation/Assets/Scripts/NeatNetwork.cs
+++ Evolution Simulation/Assets/Scripts/NeatNetwork.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 //https://www.youtube.com/watch?v=o9gI5679M_8&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=5
 public class NeatNetwork
@@ -50,12 +51,20 @@ public class NeatNetwork
             nodeId += 1;
         }
 
+        ConGene newConGene1 = new ConGene(Random.Range(0,3), Random.Range(3,5), Random.Range(0f,1f), true, 0);
+        newConGenes.Add(newConGene1);
+        ConGene newConGene2 = new ConGene(Random.Range(0,3), Random.Range(3,5), Random.Range(0f,1f), true, 1);
+        newConGenes.Add(newConGene2);
+        ConGene newConGene3 = new ConGene(Random.Range(0,3), Random.Range(3,5), Random.Range(0f,1f), true, 2);
+        newConGenes.Add(newConGene3);
+
         NeatGenome newGenome = new NeatGenome(newNodeGenes, newConGenes);
         return newGenome;
     }
 
     private void CreateNetwork()
     {
+        // Creation of Network Structure: Nodes
         foreach(NodeGene nodeGene in this.myGenome.nodeGenes)
         {
             Node newNode = new Node(nodeGene.id);
@@ -74,12 +83,14 @@ public class NeatNetwork
             }
         }
 
+        // Creation of Network Structure: Edges
         foreach(ConGene conGene in this.myGenome.conGenes)
         {
             Connection newCon = new Connection(conGene.inputNode, conGene.outputNode, conGene.weight, conGene.isActive);
             this.connections.Add(newCon);
         }
 
+        // Creation of Network Structure: Node Neighbors
         foreach(Node node in this.nodes)
         {
             foreach(Connection con in this.connections)
@@ -95,19 +106,111 @@ public class NeatNetwork
             }
         }
     }
+
+    // Main Driver Function for the NeuralNetwork
+    public float[] FeedForwardNetwork(float[] inputs)
+    {
+        float[] outputs = new float[outputNodes.Count];
+        for (int i = 0; i < inputNodes.Count; i++)
+        {
+            inputNodes[i].SetInputNodeValue(inputs[i]);
+            inputNodes[i].FeedForwardValue();
+            inputNodes[i].value = 0;    // Reset value
+        }
+        for (int i = 0; i < hiddenNodes.Count; i++)
+        {
+            hiddenNodes[i].SetHiddenNodeValue();
+            hiddenNodes[i].FeedForwardValue();
+            hiddenNodes[i].value = 0;   // Reset value
+        }
+        for (int i = 0; i < outputNodes.Count; i++)
+        {
+            outputNodes[i].SetOutputNodeValue();
+            outputs[i] = outputNodes[i].value;
+            outputNodes[i].value = 0;   // Reset value
+        }
+        //Debug.Log(outputNodes.Count);
+        return outputs;
+    }
 }
 
 //https://www.youtube.com/watch?v=o9gI5679M_8&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=5
 public class Node
 {
     public int id;
-    public float value; //stored value in the node that gets fed forward
+    // Stored value in the node that gets fed forward.
+    public float value; 
     public List<Connection> inputConnections;
     public List<Connection> outputConnections;
 
     public Node(int ident)
     {
         this.id = ident;
+        inputConnections = new List<Connection>();
+        outputConnections = new List<Connection>();
+    }
+
+    //https://www.youtube.com/watch?v=toTwSTUdL5Y&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=6
+    public void SetInputNodeValue(float val)
+    {
+        // Uses sigmoid as an activation function because the input values are never negative
+        // and sigmoid goes from 0 to 1.
+        this.value = Sigmoid(val);
+    }
+
+    //https://www.youtube.com/watch?v=toTwSTUdL5Y&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=6
+    public void SetHiddenNodeValue()
+    {
+        float val = 0;
+        foreach (Connection con in inputConnections)
+        {
+            val += (con.weight * con.inputNodeValue);
+        }
+        // Uses tanh as an activation function.
+        value = TanH(val);
+    }
+
+    //https://www.youtube.com/watch?v=toTwSTUdL5Y&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=6
+    public void SetOutputNodeValue()
+    {
+        float val = 0;
+        foreach (Connection con in inputConnections)
+        {
+            val += (con.weight * con.inputNodeValue);
+        }
+        value = TanH(val);
+    }
+
+    // Pushes forward the values into the edges for all the connections.
+    //https://www.youtube.com/watch?v=toTwSTUdL5Y&list=PLC8R0n_dfXXr0fc4NdsInilobjVhpXZk4&index=6
+    public void FeedForwardValue()
+    {
+        // Goes through each connection and stores the output of the input node
+        // as the input of the output node.
+        foreach(Connection con in this.outputConnections)
+        {
+            con.inputNodeValue = this.value;
+        }
+    }
+
+    // Activation Functions.
+    // Search for faster calculation of Sigmoid.
+    //https://en.wikipedia.org/wiki/Sigmoid_function
+    private float Sigmoid(float x) 
+    {  
+        return (1 / (1 + Mathf.Exp(-x)));  
+    }
+
+    // Search for a faster cacluation of TanH.
+    //https://de.wikipedia.org/wiki/Tangens_hyperbolicus_und_Kotangens_hyperbolicus
+    private float TanH(float x)
+    {
+        return ((2 / (1 + Mathf.Exp(-2 * x))) - 1);
+    }
+
+    private float TanHMod1(float x)
+    {
+        return ((2 / (1 + Mathf.Exp(-4 * x))) - 1);
     }
 }
 
@@ -116,14 +219,16 @@ public class Connection
 {
     public int inputNode;
     public int outputNode;
-    public float weights;
+    public float weight;
     public bool isActive;
+    public float inputNodeValue;
 
     public Connection(int inNode, int outNode, float wei, bool active)
     {
         this.inputNode = inNode;
         this.outputNode = outNode;
-        this.weights = wei;
+        this.weight = wei;
         this.isActive = active;
     }
+
 }
