2f624e9e7929b67961cce364dfa9a9cd63dc2584
Zediwan <Jeremy@qualit.ch>
2023-11-14 17:12:30 +0100

    Added stat panel

--- src_python/simulation/simulation.py
+++ src_python/simulation/simulation.py
@@ -12,12 +12,16 @@ class Simulation:
     SPAWN_THRESHOLD = 30  # Threshold to spawn new animals
     
     def __init__(self, width, height, num_animals):
+        self.width = width
+        self.height = height
+        self.panel_width = 200  # Width of the stats panel
+        self.total_width = width + self.panel_width  # Total width including the panel
+        
         pygame.init()
-        self.screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((self.total_width, height))
         pygame.display.set_caption("Evolution Simulation")
-        
+    
         num_animals = min(num_animals, self.MAX_ANIMALS)
-        
         self.animals = [
                     Animal(
                         random.randint(0, width), 
@@ -32,6 +36,7 @@ class Simulation:
                         )
                     ) for _ in range(num_animals)
         ]        
+
         self.clock = pygame.time.Clock()
 
     def run(self):
@@ -53,6 +58,9 @@ class Simulation:
                 else:
                     self.animals.remove(animal)
 
+            # Display stats
+            self.display_stats()
+            
             pygame.display.flip()
             self.clock.tick(30)
 
@@ -76,3 +84,39 @@ class Simulation:
                 self.animals.append(new_animal)
                 if len(self.animals) >= self.MAX_ANIMALS:
                     break
+                
+    def calculate_stats(self):
+        num_animals = len(self.animals)
+        if num_animals == 0:
+            return {
+                "Number of Animals": 0,
+                "Average Health": 0,
+                "Average Energy": 0,
+                # Add more stats as needed
+            }
+
+        total_health = sum(animal.health for animal in self.animals)
+        total_energy = sum(animal.energy for animal in self.animals)
+
+        return {
+            "Number of Animals": num_animals,
+            "Average Health": total_health / num_animals,
+            "Average Energy": total_energy / num_animals,
+            # Add more stats as needed
+        }
+    
+    def display_stats(self):
+        stats = self.calculate_stats()
+        font = pygame.font.SysFont('arial', 24)
+        panel_x = self.width  # Starting X coordinate of the panel
+        y_offset = 20  # Starting Y offset for text
+
+        # Draw panel background
+        pygame.draw.rect(self.screen, (230, 230, 230), (panel_x, 0, self.panel_width, self.height))
+
+        # Display each stat
+        for key, value in stats.items():
+            text = font.render(f"{key}: {value:.2f}", True, (0, 0, 0))
+            self.screen.blit(text, (panel_x + 10, y_offset))
+            y_offset += 30
+    
