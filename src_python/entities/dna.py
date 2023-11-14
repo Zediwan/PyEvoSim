55052e3a7dc0b7edbfc532561b7953eb85fd02b7
Zediwan <Jeremy@qualit.ch>
2023-11-14 16:44:46 +0100

    Added base python prototype

---
+++ src_python/entities/dna.py
@@ -0,0 +1,5 @@
+class DNA:
+    def __init__(self, max_health, max_energy, size, max_health_limit, max_energy_limit, max_size_limit):
+        self.max_health = min(max_health, max_health_limit)
+        self.max_energy = min(max_energy, max_energy_limit)
+        self.size = min(size, max_size_limit)
