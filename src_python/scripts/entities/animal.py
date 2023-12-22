1759927caccccab8beb9bb48dbcc87018a1c2b4f
Zediwan <Jeremy@qualit.ch>
2023-12-22 16:56:05 +0100

    Reworked entities to use the pygame.shape and updated collision system

--- src_python/scripts/entities/animal.py
+++ src_python/scripts/entities/animal.py
@@ -1,41 +1,38 @@
 import random
 import pygame
+from scripts.entities.dna import DNA
 
 class Animal:
-    def __init__(self, x, y, dna):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, dna: DNA):
         self.dna = dna
         self.health = dna.max_health/2
         self.energy = dna.max_energy/2
-        self.size = dna.size
         self.color = dna.color  # Use color from DNA
+        self.shape = pygame.Rect(x, y, dna.size, dna.size)
 
     def draw_bars(self, screen):
         # Health bar
-        health_bar_width = (self.health / self.dna.max_health) * self.size * 2
+        health_bar_width = (self.health / self.dna.max_health) * self.shape.width * 2
         health_bar_height = 4
-        health_bar_x = self.x - self.size
-        health_bar_y = self.y - self.size - health_bar_height - 2  # 2 pixels above the animal
+        health_bar_x = self.shape.left - self.shape.width
+        health_bar_y = self.shape.top - self.shape.height - health_bar_height - 2  # 2 pixels above the animal
         pygame.draw.rect(screen, (255, 0, 0), (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
 
         # Energy bar
-        energy_bar_width = (self.energy / self.dna.max_energy) * self.size * 2
+        energy_bar_width = (self.energy / self.dna.max_energy) * self.shape.width * 2
         energy_bar_height = 4
-        energy_bar_x = self.x - self.size
+        energy_bar_x = self.shape.left - self.shape.width
         energy_bar_y = health_bar_y - energy_bar_height - 2  # 2 pixels above the health bar
         pygame.draw.rect(screen, (0, 0, 255), (energy_bar_x, energy_bar_y, energy_bar_width, energy_bar_height))
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+        pygame.draw.rect(screen, self.color, self.shape)
         self.draw_bars(screen)
 
     def move(self):
         self.spendEnergy(1)
-
         # Random movement
-        self.x += random.randint(-5, 5)
-        self.y += random.randint(-5, 5)
+        self.shape.move_ip(random.randint(-5, 5), random.randint(-5, 5))
     
     def heal(self):
         #TODO create variables out of this
@@ -52,7 +49,7 @@ class Animal:
         return self.copy()
     
     def copy(self):
-        return Animal(self.x, self.y, self.dna.copy())
+        return Animal(self.shape.left, self.shape.top, self.dna.copy())
         
     def isAlive(self):
         return self.health > 0
--- src_python/scripts/entities/plant.py
+++ src_python/scripts/entities/plant.py
@@ -1,32 +1,31 @@
 import pygame
+from scripts.entities.dna import DNA
 
 class Plant:
-    def __init__(self, x, y, dna):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, dna: DNA):
         self.dna = dna
         self.health = dna.max_health
         self.energy = dna.max_energy
-        self.size = dna.size
         self.color = pygame.Color(0, 100, 0)  # Use color from DNA
+        self.shape = pygame.Rect(x, y, dna.size, dna.size)
 
     def draw_bars(self, screen):
         # Health bar
-        health_bar_width = (self.health / self.dna.max_health) * self.size * 2
+        health_bar_width = (self.health / self.dna.max_health) * self.shape.width * 2
         health_bar_height = 4
-        health_bar_x = self.x - self.size
-        health_bar_y = self.y - self.size - health_bar_height - 2  # 2 pixels above the animal
+        health_bar_x = self.shape.left - self.shape.width
+        health_bar_y = self.shape.top - self.shape.height - health_bar_height - 2 
         pygame.draw.rect(screen, (255, 0, 0), (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
 
         # Energy bar
-        energy_bar_width = (self.energy / self.dna.max_energy) * self.size * 2
+        energy_bar_width = (self.energy / self.dna.max_energy) * self.shape.width * 2
         energy_bar_height = 4
-        energy_bar_x = self.x - self.size
-        energy_bar_y = health_bar_y - energy_bar_height - 2  # 2 pixels above the health bar
+        energy_bar_x = self.shape.left - self.shape.width
+        energy_bar_y = health_bar_y - energy_bar_height - 2 
         pygame.draw.rect(screen, (0, 0, 255), (energy_bar_x, energy_bar_y, energy_bar_width, energy_bar_height))
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+        pygame.draw.rect(screen, self.color, self.shape)
         self.draw_bars(screen)
 
     def is_alive(self):
