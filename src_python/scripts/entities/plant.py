725d0a32352e34ecdef4ae76f42a0c91dfad8906
Zediwan <Jeremy@qualit.ch>
2023-12-22 17:37:01 +0100

    Added default values for the color variable in DNA

--- src_python/scripts/entities/plant.py
+++ src_python/scripts/entities/plant.py
@@ -5,4 +5,3 @@ from scripts.entities.organism import Organism
 class Plant(Organism):
     def __init__(self, x, y, dna: DNA):
         super().__init__(x, y, dna)
-        self.color = pygame.Color(0, 100, 0)  # Use color from DNA
