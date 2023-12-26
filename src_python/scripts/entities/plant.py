cfce36ece24e0c3f10a32f34cc9f8f4b20bd071c
Zediwan <Jeremy@qualit.ch>
2023-12-26 13:46:10 +0100

    Added constant static variable for plant photosynthesis energy gain

--- src_python/scripts/entities/plant.py
+++ src_python/scripts/entities/plant.py
@@ -3,11 +3,13 @@ from scripts.entities.dna import DNA
 from scripts.entities.organism import Organism
 
 class Plant(Organism):
+    ENERGY_GAINED_FROM_SUN = 100
+    
     def __init__(self, x, y, dna: DNA):
         super().__init__(x, y, dna)
         
     def update(self):
-        self.gainEnergy(100)
+        self.gainEnergy(Plant.ENERGY_GAINED_FROM_SUN)
         if(self.energy == self.dna.max_energy):
             self.energy /= 2
             self.shape.inflate_ip(200,200)
