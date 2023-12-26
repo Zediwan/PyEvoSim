5131e6cf518f84f6be1bb96703fc3e22d1ea9434
Zediwan <Jeremy@qualit.ch>
2023-12-26 13:05:49 +0100

    Added an abstract updated method

--- src_python/scripts/entities/plant.py
+++ src_python/scripts/entities/plant.py
@@ -5,3 +5,9 @@ from scripts.entities.organism import Organism
 class Plant(Organism):
     def __init__(self, x, y, dna: DNA):
         super().__init__(x, y, dna)
+        
+    def update(self):
+        self.gainEnergy(100)
+        if(self.energy == self.dna.max_energy):
+            self.energy /= 2
+            self.shape.inflate_ip(200,200)
