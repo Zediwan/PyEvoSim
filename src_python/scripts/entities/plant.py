1eb3716d2771ed1122e78048e38cf6e6b3eb8e15
Zediwan <Jeremy@qualit.ch>
2024-01-06 17:34:42 +0100

    Added plant growth

--- src_python/scripts/entities/plant.py
+++ src_python/scripts/entities/plant.py
@@ -1,9 +1,10 @@
 import pygame
+import random
 from scripts.entities.dna import DNA
 from scripts.entities.organism import Organism
 
 class Plant(Organism):
-    ENERGY_GAINED_FROM_SUN = .001
+    ENERGY_GAINED_FROM_SUN = 0.1
     
     def __init__(self, x, y, dna: DNA):
         super().__init__(x, y, dna)
@@ -11,5 +12,9 @@ class Plant(Organism):
     def update(self):
         self.gainEnergy(Plant.ENERGY_GAINED_FROM_SUN)
         if(self.energy == self.calculate_max_energy()):
-            self.spendEnergy(10)
-            #self.shape.inflate_ip(.1,.1)
+            self.grow()
+
+    def grow(self):
+        self.spendEnergy(20)
+        self.shape.inflate_ip(random.random()*1.1,random.random()*1.1)
+        
