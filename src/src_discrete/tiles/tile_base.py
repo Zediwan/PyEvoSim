8f2c464cf7be4a266eadf38b8bafb6d376876278
Zediwan <Jeremy@qualit.ch>
2024-03-02 17:54:53 +0100

    Neighbour tiles 2.0

--- src/src_discrete/tiles/tile_base.py
+++ src/src_discrete/tiles/tile_base.py
@@ -15,3 +15,13 @@ class Tile():
     def draw(self, screen : pygame.Surface):
         pygame.draw.rect(screen, pygame.Color("black"), self.rect, tile_outline_thickness)
         pygame.draw.rect(screen, self.color, self.rect)
+        
+    # # Getter & Setter  
+    # def addNeighbour(self, direction, tile):
+    #     self.neighbours[direction] = tile
+
+    # def getNeighbour(self, direction):
+    #     return self.neighbours[direction]
+
+    # def getDirections(self):
+    #     return list(self.neighbours.keys())
