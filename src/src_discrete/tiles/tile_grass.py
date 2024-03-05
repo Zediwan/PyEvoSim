7da87b248edea2d445432c2b401e42a594e35c2e
Zediwan <Jeremy@qualit.ch>
2024-03-05 23:00:30 +0100

    Added variables for grass growth level

--- src/src_discrete/tiles/tile_grass.py
+++ src/src_discrete/tiles/tile_grass.py
@@ -4,12 +4,12 @@ from config import *
 
 class GrassTile(Tile):
     
-    def __init__(self, rect: pygame.Rect, cell_size: int, value: int = 0):
+    def __init__(self, rect: pygame.Rect, cell_size: int, value: int = MIN_GRASS_VALUE):
         super().__init__(rect, cell_size)
         self.growth_value = value
         
     def update(self):
-        self.color = dirt_color.lerp(min_grass_color, self.growth_value / 10).lerp(max_grass_color, self.growth_value / 10)
+        self.color = dirt_color.lerp(min_grass_color, self.growth_value / MAX_GRASS_VALUE).lerp(max_grass_color, self.growth_value / MAX_GRASS_VALUE)
         
     def get_value(self):
         return self.growth_value
