b66160eb808ce224060f161ee4b5340b121f22b9
Zediwan <Jeremy@qualit.ch>
2024-03-05 23:00:47 +0100

    Initiated the surface grid class

---
+++ src/src_discrete/grids/grid_surface.py
@@ -0,0 +1,12 @@
+from grids.grid_base import Grid
+from tiles.tile_base import Tile
+import pygame
+
+class GridSurface(Grid):
+    def __init__(self, rows: int, cols: int, tile_size: int):
+        super().__init__(rows, cols, tile_size)
+        
+    def create_tile(self, col, row) -> Tile:
+        rect = pygame.Rect(col * self.tile_size, row * self.tile_size, self.tile_size, self.tile_size)
+        #return Tile(rect, self.tile_size)
+        return super().create_tile(col, row)
