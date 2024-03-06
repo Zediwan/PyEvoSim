afac6320d22a3518313019d85ff9e4537ec73366
Zediwan <Jeremy@qualit.ch>
2024-03-06 22:46:17 +0100

    Added a copy and mutate method to bounded variable

--- src/src_discrete/bounded_variable.py
+++ src/src_discrete/bounded_variable.py
@@ -1,5 +1,14 @@
+import random
+
 class BoundedVariable:
     def __init__(self, value: int, min_val: int, max_val: int):
+        if min_val >= max_val:
+            raise ValueError("Minimum value must be less than maximum value.")
+        if value < min_val:
+            value = min_val
+        elif value > max_val:
+            value = max_val
+        
         self._value = value
         self._min_val = min_val
         self._max_val = max_val
@@ -16,3 +25,14 @@ class BoundedVariable:
             self._value = self._max_val
         else:
             self._value = new_value
+            
+    def mutate(self):
+        if self._value == self._min_val:
+            self._value += 1
+        elif self._value == self._max_val:
+            self._value -= 1
+        else:
+            self._value += random.choice([-1, 1])
+            
+    def copy(self):
+        return BoundedVariable(self._value, self._min_val, self._max_val)
