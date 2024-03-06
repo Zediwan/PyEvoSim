e82c9cf9281a10237919aae0d536d51acc20d474
Zediwan <Jeremy@qualit.ch>
2024-03-06 21:13:14 +0100

    Added perlin noise based world generation

--- src/src_discrete/grids/grid_ground.py
+++ src/src_discrete/grids/grid_ground.py
@@ -14,12 +14,12 @@ class GridGround(Grid):
     def create_tile(self, col, row) -> Tile:
         rect = pygame.Rect(col * self.tile_size, row * self.tile_size, self.tile_size, self.tile_size)
         
-        #n = noise.pnoise2(row * self.tile_size, col * self.tile_size)
-        n = random.random()
+        n = noise.pnoise2(row / 20.0, col / 24.0)
+        #n = random.random()
         
         if SURROUNDED_BY_WATER and (row == 0 or col == 0 or row == self.rows - 1 or col == self.cols - 1):
             tile : Tile = WaterTile(rect, self.tile_size, 10)
-        elif n >= LAND_PERCENTAGE:
+        elif n <= WATER_PERCENTAGE:
             tile : Tile = WaterTile(rect, self.tile_size, random.randint(0,10))
         else:
             tile : Tile = GrassTile(rect, self.tile_size, random.randint(0,10))
