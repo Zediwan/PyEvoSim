cc5b442e39ff5c1b77ac89239fced25778424d21
Zediwan <Jeremy@qualit.ch>
2024-03-06 20:11:46 +0100

    Updated world class

--- src/src_discrete/world.py
+++ src/src_discrete/world.py
@@ -6,21 +6,23 @@ class World():
     def __init__(self, height : int, width : int, tile_size : int):
         self.height = height
         self.width = width
-        self.grid_size = tile_size
+        self.tile_size = tile_size
         
-        rows = math.floor(height / tile_size)
-        cols = math.floor(width / tile_size)
+        self.height, self.width = World.adjust_dimensions(self.height, self.width, self.tile_size)
+        
+        rows = math.floor(self.height / self.tile_size)
+        cols = math.floor(self.width / self.tile_size)
         
         self.ground = GridGround(rows , cols, tile_size)
-        #self.surface = Grid(rows , cols, tile_size)
-        #self.sky = Grid(rows , cols, tile_size)
         
     def update(self):
         self.ground.update()
-        #self.surface.update()
-        #self.sky.update()
         
     def draw(self, screen : pygame.Surface):
         self.ground.draw(screen)
-        #self.surface.draw(screen)
-        #self.sky.draw(screen)
+        
+    @staticmethod
+    def adjust_dimensions(height, width, tile_size):
+        adjusted_height = (height // tile_size) * tile_size
+        adjusted_width = (width // tile_size) * tile_size
+        return adjusted_height, adjusted_width
