5c36bc290d56c66846ac852f7f1044f33771d29e
Zediwan <Jeremy@qualit.ch>
2024-03-06 21:55:13 +0100

    Restructured settings from config to animals

--- src/src_discrete/entities/animal.py
+++ src/src_discrete/entities/animal.py
@@ -1,3 +1,4 @@
+import math
 import random
 from pygame import Color, Rect, Surface
 from entities.organism import Organism
@@ -7,7 +8,14 @@ from tiles.tile_water import WaterTile
 from tiles.tile_base import Tile
 
 class Animal(Organism):
-    def __init__(self, tile: Tile, shape: Rect|None = None, color: Color = ANIMAL_COLOR, health: float = BASE_ANIMAL_HEALHT, energy: float = BASE_ANIMAL_ENERGY):
+    ANIMAL_COLOR = pygame.Color("black")
+    
+    MIN_ANIMAL_HEALTH, MAX_ANIMAL_HEALTH = Organism.MIN_ORGANISM_HEALTH, Organism.MAX_ORGANISM_HEALTH
+    MIN_ANIMAL_ENERGY, MAX_ANIMAL_ENERGY = Organism.MIN_ORGANISM_ENERGY, Organism.MAX_ORGANISM_ENERGY
+
+    BASE_ANIMAL_HEALTH = MAX_ANIMAL_HEALTH - 50
+    BASE_ANIMAL_ENERGY = MAX_ANIMAL_ENERGY - 50
+    def __init__(self, tile: Tile, shape: Rect|None = None, color: Color = ANIMAL_COLOR, health: int = BASE_ANIMAL_HEALTH, energy: int = BASE_ANIMAL_ENERGY):
         if not shape:
             shape = tile.rect
             
@@ -17,20 +25,20 @@ class Animal(Organism):
         
     def update(self):
         super().update()
-        self.color = pygame.Color("grey").lerp(ANIMAL_COLOR, min(self.health_ratio(),.8))
+        self.color = pygame.Color("grey").lerp(self.ANIMAL_COLOR, min(self.health_ratio(),.8))
         
         if isinstance(self.tile, WaterTile):
             self.loose_health(self.tile.water_value * 10) 
         elif isinstance(self.tile, GrassTile):
             if self.tile.growth_value >= 1:
-                self.gain_health(self.tile.growth_value * 5)
-                self.tile.growth_value -= min(1, self.tile.growth_value)
+                self.gain_enery(math.floor(self.tile.growth_value / 2))
+                self.tile.growth_value -= min(2, self.tile.growth_value)
         
         direction = self.think()
         if direction:
             self.enter_tile(direction)
             
-        if self.health >= 80:
+        if self.health_ratio() >= .9:
             unoccupied_neighbor = self.tile.get_random_unoccupied_neighbor()
             if unoccupied_neighbor:
                 if random.random() <= .01:
@@ -53,7 +61,7 @@ class Animal(Organism):
         return Animal(tile)
     
     def health_ratio(self):
-        ratio = self.health / MAX_ANIMAL_HEALTH
+        ratio = self.health / self.MAX_ANIMAL_HEALTH
         assert ratio <= 1, "Health ratio is bigger than 1."
         assert ratio >= 0, "Health ratio is smaller than 0."
         return ratio
--- src/src_discrete/entities/organism.py
+++ src/src_discrete/entities/organism.py
@@ -6,7 +6,15 @@ from config import *
 import tiles.tile_base as tile
 
 class Organism(ABC):
-    def __init__(self, tile: tile.Tile, shape: pygame.Rect, color: pygame.Color, health: float = BASE_ORGANISM_HEALHT, energy: float = BASE_ORGANISM_ENERGY):
+    ORGANISM_MAINTANANCE_ENERGY = 10
+
+    MIN_ORGANISM_HEALTH, MAX_ORGANISM_HEALTH = 0, 100
+    MIN_ORGANISM_ENERGY, MAX_ORGANISM_ENERGY = 0, 100
+
+    BASE_ORGANISM_HEALTH = MAX_ORGANISM_HEALTH
+    BASE_ORGANISM_ENERGY = MAX_ORGANISM_ENERGY
+    
+    def __init__(self, tile: tile.Tile, shape: pygame.Rect, color: pygame.Color, health: int = BASE_ORGANISM_HEALTH, energy: int = BASE_ORGANISM_ENERGY):
         self.health = health
         self.energy = energy
         self.shape = shape
@@ -20,21 +28,20 @@ class Organism(ABC):
             self.die()
             return
         
-        if self.energy <= 0:
-            self.loose_health(1)
-        else:
-            self.use_enery(1) #TODO: make this a variable in config
+        self.use_enery(self.ORGANISM_MAINTANANCE_ENERGY) #TODO: make this a variable in config
             
         self.invariant()
     
     @abstractmethod
     def draw(self, screen: pygame.Surface):
-        assert self.is_alive(), "Animal is being drawn despite being dead"
+        if not self.is_alive():
+            raise ValueError("Animal is being drawn despite being dead")
         self.tile.temp_surface.fill(self.color)
         pass
     
     def enter_tile(self, tile: tile.Tile):
-        assert not tile.is_occupied(), "Tile is already occupied."
+        if tile.is_occupied():
+            raise ValueError("Tile is already occupied.")
         
         try:
             self.tile.leave()
@@ -45,43 +52,43 @@ class Organism(ABC):
         
         self.invariant()
         
-    # def health_ratio(self):
-    #     ratio = self.health / MAX_ORGANISM_HEALTH
-    #     assert ratio <= 1, "Health ratio is bigger than 1."
-    #     assert ratio >= 0, "Health ratio is smaller than 0."
-    #     return ratio
-    
     def set_health(self, new_health):
-        assert new_health >= MIN_ORGANISM_HEALTH, "New health is below organism min."
-        assert new_health <= MAX_ORGANISM_HEALTH, "New health is above organism max."
+        if new_health < self.MIN_ORGANISM_HEALTH:
+            raise ValueError("New health is below organism min.")
+        if new_health > self.MAX_ORGANISM_HEALTH:
+            raise ValueError("New health is above organism max.")
         
         self.health = new_health
         
     def gain_enery(self, energy_gained):
-        assert energy_gained >= 0, "Energy gained is negative."
+        if energy_gained < 0:
+            raise ValueError("Energy gained is negative.")
         
         dif = self.energy + energy_gained
-        self.energy = min(dif, MAX_ORGANISM_ENERGY)
-        if dif > MAX_ORGANISM_ENERGY:
-            self.gain_health(dif - MAX_ORGANISM_ENERGY)
+        self.energy = min(dif, self.MAX_ORGANISM_ENERGY)
+        if dif > self.MAX_ORGANISM_ENERGY:
+            self.gain_health(dif - self.MAX_ORGANISM_ENERGY)
        
     def use_enery(self, energy_used):
-        assert energy_used >= 0, "Energy used is negative."
+        if energy_used < 0:
+            raise ValueError("Energy used is negative.")
         
         dif = self.energy - energy_used
-        self.energy = max(dif, MAX_ORGANISM_ENERGY)
-        if dif <= MIN_ANIMAL_ENERGY:
-            self.use_enery(-dif)     
+        self.energy = max(dif, self.MAX_ORGANISM_ENERGY)
+        if dif <= self.MIN_ORGANISM_ENERGY:
+            self.loose_health(-dif)     
         
     def gain_health(self, health_gained):
-        assert health_gained >= 0, "Health gained is negative."
+        if health_gained < 0:
+            raise ValueError("Health gained is negative.")
         dif = self.health + health_gained
-        self.health = min(dif, MAX_ORGANISM_HEALTH)  
+        self.health = min(dif, self.MAX_ORGANISM_HEALTH)  
         
     def loose_health(self, health_lost):
-        assert health_lost >= 0, "Health lost is negative."
+        if health_lost < 0:
+            raise ValueError("Health lost is negative.")
         dif = self.health - health_lost
-        self.health = max(dif, MIN_ANIMAL_HEALHT) 
+        self.health = max(dif, self.MIN_ORGANISM_HEALTH) 
         
     def is_alive(self) -> bool:
         is_alive = self.health > 0
@@ -90,7 +97,8 @@ class Organism(ABC):
         return is_alive
     
     def die(self):
-        assert self.health <= 0, "Organism tries to die despite not being dead."
+        if self.health > 0:
+            raise ValueError("Organism tries to die despite not being dead.")
         self.tile.leave()
     
     @abstractmethod
@@ -98,5 +106,7 @@ class Organism(ABC):
         pass 
     
     def invariant(self):
-        assert self.tile, "Organism does not have a tile!"
-        assert self.tile.organism == self, "Tiles Organism and Organisms tile are not equal."
+        if not self.tile:
+            raise ValueError("Organism does not have a tile!")
+        if self.tile.organism != self:
+            raise ValueError("Tiles Organism and Organisms tile are not equal.")
