075c2fadf81f47ecd04568f4681f4995eaeb7ce7
Zediwan <Jeremy@qualit.ch>
2024-03-07 15:25:02 +0100

    Improved water flow algorithm

--- src/src_discrete/tiles/tile_water.py
+++ src/src_discrete/tiles/tile_water.py
@@ -1,29 +1,43 @@
 from pygame import Color, Rect
 from tiles.tile_ground import GroundTile
 from config import *
+import random
 
 class WaterTile(GroundTile):
     MIN_WATER_VALUE, MAX_WATER_VALUE= 0, 10
     STARTING_WATER_LEVEL = 8
-    DOES_WATER_FLOW = False
-    WATER_FLOW_VALUE = 1
+    WATER_FLOW_AT_BORDER = 1
+    DOES_WATER_FLOW = True
+    WATER_FLOW_BETWEEN_TILES = 1
     MIN_WATER_COLOR = Color(204, 229, 233, ground_alpha)
     MAX_WATER_COLOR = Color(26, 136, 157, ground_alpha)
     
     def __init__(self, rect: Rect, cell_size: int, value: int = STARTING_WATER_LEVEL):
         super().__init__(rect, cell_size)
-        self.water_value = value
+        self.water_value: int = value
         self.updateColor()
         
     def update(self):
         super().update()
         
         if self.DOES_WATER_FLOW:
-            neigbour = self.get_random_neigbor()
-            if isinstance(neigbour, WaterTile):
-                if self.water_value >= neigbour.water_value:
-                    self.transfer_water(self.WATER_FLOW_VALUE, neigbour) 
-        
+            if self.is_border_tile:
+                self.set_value(min(self.water_value + self.WATER_FLOW_AT_BORDER, self.MAX_WATER_VALUE))
+            
+            lowest_water_tile = None
+            lowest_water_value = self.MAX_WATER_VALUE + 1
+            
+            for neighbour in self.neighbours.values():
+                if isinstance(neighbour, WaterTile) and neighbour.water_value <= lowest_water_value:
+                    if random.random() >= 0.5:
+                        lowest_water_tile = neighbour
+                        lowest_water_value = neighbour.water_value
+
+            if lowest_water_tile is not None:
+                difference_to_neighbor = self.water_value - lowest_water_tile.water_value
+                if difference_to_neighbor > 0:
+                    self.transfer_water(min(self.WATER_FLOW_BETWEEN_TILES, difference_to_neighbor), lowest_water_tile)
+
         self.invariant()
         
     def updateColor(self):
