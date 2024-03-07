8c748dd742a2a16ed75b985a3692f26435fa973f
Zediwan <Jeremy@qualit.ch>
2024-03-07 17:13:58 +0100

    Added bounded var to simulation

--- src/src_discrete/simulation.py
+++ src/src_discrete/simulation.py
@@ -1,14 +1,15 @@
 import config
 import pygame as pg
 import sys
+from bounded_variable import BoundedVariable
 from world import World
 class Simulation:
-    STARTING_GAME_SPEED = 120
+    STARTING_GAME_SPEED: int = 120
     
-    SMALL_BIG_GAME_SPEED_THRESHOLD = 10
-    BIG_CHANGE_GAME_SPEED = 10
-    SMALL_CHANGE_GAME_SPEED = 1
-    MIN_GAME_SPEED = 1
+    SMALL_BIG_GAME_SPEED_THRESHOLD: int = 10
+    BIG_CHANGE_GAME_SPEED: int = 10
+    SMALL_CHANGE_GAME_SPEED: int = 1
+    MIN_GAME_SPEED: int = 1
     
     def __init__(self, height: int, width: int, tile_size : int):
         pg.display.init()
@@ -18,7 +19,7 @@ class Simulation:
         self.clock = pg.time.Clock()
         self.world = World(height, width, tile_size)
         
-        self.game_speed = self.STARTING_GAME_SPEED
+        self.game_speed: BoundedVariable = BoundedVariable(self.STARTING_GAME_SPEED, self.MIN_GAME_SPEED, 120)
         self.increase_game_speed = False
         self.decrease_game_speed = False
 
@@ -69,17 +70,17 @@ class Simulation:
                 self.world.update()
                 self.world.draw(self.screen) 
                 pg.display.flip() 
-                self.clock.tick(self.game_speed)  
+                self.clock.tick(self.game_speed.value)  
             
     def handle_game_speed(self):
-        if self.game_speed <= self.SMALL_BIG_GAME_SPEED_THRESHOLD:
-            change_in_game_speed = self.SMALL_CHANGE_GAME_SPEED
-        elif self.game_speed > self.SMALL_BIG_GAME_SPEED_THRESHOLD:
-            change_in_game_speed = self.BIG_CHANGE_GAME_SPEED
+        if self.game_speed.value <= self.SMALL_BIG_GAME_SPEED_THRESHOLD:
+            change_in_game_speed: int = self.SMALL_CHANGE_GAME_SPEED
+        else:
+            change_in_game_speed: int = self.BIG_CHANGE_GAME_SPEED
                 
         if self.increase_game_speed:
-            self.game_speed += change_in_game_speed
+            self.game_speed.add_value(change_in_game_speed)
             #print(self.game_speed)
         if self.decrease_game_speed:
-            self.game_speed = max(self.game_speed - change_in_game_speed, self.MIN_GAME_SPEED)
+            self.game_speed.add_value(-change_in_game_speed)
             #print(self.game_speed)   
