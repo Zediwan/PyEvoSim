6cdf094a13e61803a2313058be5e670e3c2d7936
Zediwan <Jeremy@qualit.ch>
2024-03-07 20:06:21 +0100

    Refactored bounded variable class

--- src/src_discrete/bounded_variable.py
+++ src/src_discrete/bounded_variable.py
@@ -1,17 +1,13 @@
+import copy
 import random
 
 class BoundedVariable:
     def __init__(self, value: int, min_val: int, max_val: int):
         if min_val >= max_val:
             raise ValueError("Minimum value must be less than maximum value.")
-        if value < min_val:
-            value = min_val
-        elif value > max_val:
-            value = max_val
-        
-        self._value = value
         self._min_val = min_val
         self._max_val = max_val
+        self.value = value
 
     @property
     def value(self) -> int:
@@ -27,14 +23,8 @@ class BoundedVariable:
             self._value = new_value
     
     def add_value(self, add_val: int):
-        new_value = self._value + add_val
-        if new_value < self._min_val:
-            self._value = self._min_val
-        elif new_value > self._max_val:
-            self._value = self._max_val
-        else:
-            self._value = new_value
-        return max(new_value - self._max_val, 0)
+        self.value += add_val
+        return max(self._value - self._max_val, 0)
             
     def ratio(self) -> float:
         total_range = self._max_val - self._min_val
@@ -42,12 +32,7 @@ class BoundedVariable:
         return current_difference / total_range
             
     def mutate(self):
-        if self._value == self._min_val:
-            self._value += 1
-        elif self._value == self._max_val:
-            self._value -= 1
-        else:
-            self._value += random.choice([-1, 1])
+        self.add_value(random.choice([-1, 1]))
             
     def copy(self):
-        return BoundedVariable(self._value, self._min_val, self._max_val)
+        return copy.copy(self)
