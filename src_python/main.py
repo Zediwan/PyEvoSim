3b17486be4a1c579bca66af1a6c47c85dfe1f851
Zediwan <Jeremy@qualit.ch>
2024-03-11 17:24:09 +0100

    Updated folder structure and gitignore

--- src_python/main.py
+++
@@ -1,7 +0,0 @@
-from scripts.simulation.simulation import Simulation
-
-if __name__ == "__main__":
-    WIDTH, HEIGHT = 1920, 1080
-    NUM_ANIMALS = 10
-    simulation = Simulation(WIDTH, HEIGHT, NUM_ANIMALS)
-    simulation.run()
