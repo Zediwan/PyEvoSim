e15f1fb825207fc08c0753392a84cfdff086ede5
Zediwan <Jeremy@qualit.ch>
2024-03-13 12:17:28 +0100

    Added get neigbor direction to get a direction neighboring the given direction

--- src/config.py
+++ src/config.py
@@ -59,3 +59,16 @@ class Direction(ExtendedEnum):
     @staticmethod
     def is_valid_direction(direction : Direction) -> bool:
         return direction in Direction.get_options()
+    
+    @staticmethod
+    def get_neighboring_directions(direction: Direction) -> list[Direction]:
+        if direction == Direction.NORTH:
+            return [Direction.WEST, Direction.EAST]
+        elif direction == Direction.EAST:
+            return [Direction.NORTH, Direction.SOUTH]
+        elif direction == Direction.SOUTH:
+            return [Direction.WEST, Direction.EAST]
+        elif direction == Direction.WEST:
+            return [Direction.NORTH, Direction.SOUTH]
+        else:
+            raise ValueError("Invalid direction provided")
