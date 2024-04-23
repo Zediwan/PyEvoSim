10c429f4c130c99e620edd8c310e7c54d1f2a2dd
Zediwan <Jeremy@qualit.ch>
2024-04-23 16:21:51 +0200

    tweaked animal variables

--- src/animal.py
+++ src/animal.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Optional
 from pygame import Color, Rect, Surface
-from pygame.math import clamp
+from pygame.math import clamp, lerp
 from random import random, randint, shuffle
 
 from config import *
@@ -24,18 +24,19 @@ class Animal(Organism):
         if not color:
             color = pygame.Color(randint(20,230), randint(20,230), randint(20,230))
             
-        health = self.MAX_HEALTH * clamp(random(), 0.4, 0.6)
-        energy = self.MAX_ENERGY * clamp(random(), 0.4, 0.6)
+        health = self.MAX_HEALTH * lerp(0.4, 0.6, random())
+        energy = self.MAX_ENERGY * lerp(0.4, 0.6, random())
         super().__init__(tile, shape, color, health, energy)
         
     def update(self):
         super().update()
+        self.use_energy(2)
         
         DROWNING_DAMAGE = 10
         if self.tile.has_water:
             self.loose_health(DROWNING_DAMAGE)
             
-        damage = 2
+        damage = 10
         wants_to_eat = self.energy_ratio() < .9 and self.health_ratio() < .9
         if self.tile.has_plant() and wants_to_eat:
             self.tile.plant.loose_health(damage)
@@ -97,7 +98,7 @@ class Animal(Organism):
         
     ########################## Reproduction #################################
     def reproduce(self):
-        MIN_REPRODUCTION_HEALTH = .75
+        MIN_REPRODUCTION_HEALTH = .25
         MIN_REPRODUCTION_ENERGY = .5
         REPRODUCTION_CHANCE = .001
         if (self.health_ratio() >= MIN_REPRODUCTION_HEALTH and
@@ -105,7 +106,7 @@ class Animal(Organism):
             random() <= REPRODUCTION_CHANCE):
             unoccupied_neighbor = self.tile.get_random_neigbor(no_animal = True, no_water = True)
             if unoccupied_neighbor:
-                REPRODUCTION_ENERGY_COST = self.MAX_ENERGY / 3
+                REPRODUCTION_ENERGY_COST = self.MAX_ENERGY / 2
                 self.use_energy(REPRODUCTION_ENERGY_COST)
                 offspring = self.copy(unoccupied_neighbor)
                 offspring.mutate()
