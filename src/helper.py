8a745f0530358e360bb2f3ee0bb614a04ea707b5
Zediwan <Jeremy@qualit.ch>
2024-04-27 23:28:13 +0200

    imporved helper method to support none values not being displayed as empty strings

--- src/helper.py
+++ src/helper.py
@@ -1,10 +1,13 @@
-def format_number(value: float) -> str:
-    num = round(value)
-    s = f"{num}"
-    if num >= 1000000:
-        num = round(num/1000000,1)
-        s = f"{num}m"
-    elif num >= 1000:
-        num = round(num/1000,1)
-        s = f"{num}k"
-    return s 
+def format_number(value: float | int) -> str:
+    if value is not None:
+        num = round(value, 2)
+        s = f"{num}"
+        if num >= 1000000:
+            num = round(num/1000000, 2)
+            s = f"{num}m"
+        elif num >= 1000:
+            num = round(num/1000, 2)
+            s = f"{num}k"
+        return s 
+    else:
+        return ""
