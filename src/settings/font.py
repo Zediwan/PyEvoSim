b973bd49b9e832a66c8777740b17364576e7b0e8
Zediwan <Jeremy@qualit.ch>
2024-05-02 13:47:35 +0200

    added todos

--- src/settings/font.py
+++ src/settings/font.py
@@ -1,6 +1,8 @@
 import pygame
 from settings.config import TILE_SIZE
 
+# TODO add all the fonts
+# TODO is this still needed?
 pygame.font.init()
 font_size = int(1.2 * TILE_SIZE)
 font: pygame.font.Font = pygame.font.Font(None, font_size) 
