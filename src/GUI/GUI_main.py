dbc208ee6300f2d4d52c1971c5b73cd400bcfaa4
andrin.M <andrin.mueller@students.unibe.ch>
2024-05-15 20:49:47 +0200

    searchbar changing position fixed + themed GUI

--- src/GUI/GUI_main.py
+++ src/GUI/GUI_main.py
@@ -2,66 +2,66 @@ import tkinter as tk
 import pandas as pd
 from pandasql import sqldf
 from pandastable import Table
+from tkinter import ttk
+from ttkthemes import ThemedTk
 
-#Define global function for querrying
-pysqldf = lambda q: sqldf(q, globals())
-file_path_Andrin = 'C:\\Users\\andri\\OneDrive - Universitaet Bern\\6. Semester\\Programming for Data Science\\EvolutionSimulation-1\\src\\GUI\\organism_database_20240428124116.csv'
-file_path_Milos = 'C:\\Users\Milos-Uni\EvolutionSimulation\databases\organism_database_20240428124116.csv'
-# Read the csv into a panda df
-db = pd.read_csv(file_path_Andrin)
 
-# Create the main window
-root = tk.Tk()
-root.title("Evolution Simulation")
 
-# Define frame globally
-frame = tk.Frame(root)
-frame.pack(fill='both', expand=True)
+#Read the csv into a panda df
+db = pd.read_csv("src/GUI/organism_database_20240428124116.csv")
 
+#Takes sql statement from user and returns the querried pandadf
 def querry_input():
-    # Retrieve the input from the entry widget
+    #Retrieve the input from the entry widget
     user_querry = entry.get()
 
-    # Pass input to a pandasql and get the result
+    #Pass input to a pandasql and get the result
     output = pysqldf(user_querry)
 
     #update the table
     update_table(output)
 
-    # Print the result to the console
-    #print(output)
-
+#Allows user to make a new querry
 def new_query():
-    # Clear the entry widget
+    #Clear the entry widget
     entry.delete(0, tk.END)
 
     # Reset the result label
     update_table(db)
 
-# Update the table with new data
+#Update the table with new data
 def update_table(dataframe):
-    global frame
-    frame.destroy()
+    table.model.df = dataframe
+    table.redraw()
 
-    frame = tk.Frame(root)
-    frame.pack(fill='both', expand=True)
-    pt = Table(frame, dataframe=dataframe)
-    pt.show()
+#Define global function for querrying
+pysqldf = lambda q: sqldf(q, globals())
 
-# entry widget for user input
-entry = tk.Entry(root, width=40)
+
+#Create the main window
+#root = tk.Tk()
+root = ThemedTk(theme = "arc")
+root.title("Evolution Simulation")
+
+#Define frame globally
+frame = ttk.Frame(root)
+frame.pack(fill='both', expand=True)
+
+#entry widget for user input
+entry = ttk.Entry(root, width=40)
 entry.pack(pady=10)
 
-# submit button
-submit_button = tk.Button(root, text="Submit", command=querry_input)
+#submit button
+submit_button = ttk.Button(root, text="Submit", command=querry_input)
 submit_button.pack(pady=5)
 
-# "New Query" button
-new_query_button = tk.Button(root, text="New Query", command=new_query)
+#"restart" button
+new_query_button = ttk.Button(root, text="restart", command=new_query)
 new_query_button.pack(pady=5)
 
-# Initial table display
+#initial table display
 table = Table(frame, dataframe=db)
+table.setTheme("dark")
 table.show()
 
 root.mainloop()
