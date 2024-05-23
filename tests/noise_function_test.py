b74b225b4ebdd2a6e3182ecec076541c8fd3e678
Zediwan <Jeremy@qualit.ch>
2024-05-23 08:57:33 +0200

    Wrote another test for noise_function

--- tests/noise_function_test.py
+++ tests/noise_function_test.py
@@ -18,4 +18,22 @@ class TestWeigh(unittest.TestCase):
         
         result = NoiseFunction.weigh(x, y, [function1, function2])
         # Assert the result is within the expected range
-        self.assertEqual(expected_result, result, f"Function noise output does not match. {expected_result} != {result}")
+        self.assertEqual(expected_result, result, "Function noise output does not match.")
+
+    def test_weighted_average_with_custom_weights(self):
+        # Create two NoiseFunction instances
+        function1 = NoiseFunction(factor_x=2, factor_y=3, offset_x=1, offset_y=2, pow_x=1, pow_y=2, pow=1.5, fudge=1.2)
+        function2 = NoiseFunction(factor_x=1.5, factor_y=2.5, offset_x=-1, offset_y=-2, pow_x=2, pow_y=1, pow=1.2, fudge=1.5)
+
+        weigth_f1 = 1
+        weigth_f2 = 2
+
+        x = 1
+        y = 1
+        result_f1 = function1.noise(x, y) * weigth_f1
+        result_f2 = function2.noise(x, y) * weigth_f2
+        expected_result = (result_f1 + result_f2) / (weigth_f1 + weigth_f2)
+
+        result = NoiseFunction.weigh(x, y, [function1, function2], [weigth_f1, weigth_f2])
+        # Assert the result is within the expected range
+        self.assertEqual(expected_result, result, "Function noise output does not match.")
