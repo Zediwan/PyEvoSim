da334bba4b4093fe3241fb8be7fe7e7862020413
Zediwan <Jeremy@qualit.ch>
2024-05-23 17:02:20 +0200

    Created a second test for setting headers

--- tests/database_manager_test.py
+++ tests/database_manager_test.py
@@ -3,6 +3,14 @@ from code.database_manager import DatabaseManager
 
 
 class DatabaseManagerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dbm: DatabaseManager = None
+
+    def tearDown(self):
+        import os
+        if self.dbm:
+            os.remove(self.dbm.csv_pathname)
+        
     def test_csv_creation(self):
         """
         This test checks if the csv is created properly
@@ -12,8 +20,26 @@ class DatabaseManagerTest(unittest.TestCase):
         headers: list = []
         headers.append("h1"),
         headers.append("h2")
-        dbm = DatabaseManager(headers)
-        path = dbm.csv_pathname
+        self.dbm = DatabaseManager(headers)
         
-        self.assertTrue(os.path.isfile(path), "CSV was not created")
-        os.remove(path)
+        self.assertTrue(os.path.isfile(self.dbm.csv_pathname), "CSV was not created")
+        
+    def test_header_setting(self):
+        """"
+        Thist test checks if the headers are set properly
+        """
+        headers: list = []
+        headers.append("h1"),
+        headers.append("h2")
+        self.dbm = DatabaseManager(headers)
+        
+        import csv
+        # Open the CSV file
+        with open(self.dbm.csv_pathname, 'r') as file:
+            # Create a CSV reader object
+            csv_reader = csv.reader(file)
+            
+            # Read the first row
+            header = next(csv_reader)
+            
+            self.assertEqual(header, headers, "Headers are not equal")
