d41feb00313107bde3e69e161c3e8bd285bc172a
Zediwan <Jeremy@qualit.ch>
2024-05-23 13:22:10 +0200

    fixed path name

--- code/database/functions.py
+++ code/database/functions.py
@@ -6,7 +6,7 @@ from pandasql import sqldf
 from pandastable import Table
 
 def read_csv() :
-    db = pd.read_csv("organism_database_20240428124116.csv", header=None,
+    db = pd.read_csv("code/database/organism_database_20240428124116.csv", header=None,
                      names=["Type", "ID", "Birth_Time_ms", "Deathtime_Time_ms", "Time_lived", "Max_Health", "Updates", "Health_Ratio", "Energy", "Max_Energy", "Energy_Ratio", "Tiles_Travel",
                             "Attack_Power", "Organism_Attacked", "Animals_Kill", "Plants_Killed", "Number_Offspring", "R", "G", "B"])
     assert db is not None
