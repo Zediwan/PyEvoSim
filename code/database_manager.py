12d9efa88c6577787bfc227389766477c19e67b3
Zediwan <Jeremy@qualit.ch>
2024-05-23 17:31:31 +0200

    Added a test to check behavior when insuficent data is provide

--- code/database_manager.py
+++ code/database_manager.py
@@ -14,15 +14,17 @@ class DatabaseManager():
         self.csv_pathname: str = DatabaseManager.folder_pathname + DatabaseManager.filename_start + self.name + DatabaseManager.filename_end
 
         self.metadata: dict[str] = {}
-        
+        self.headers = headers
+
         try:
             with open(self.csv_pathname, mode="a", newline="") as file:
                 writer: csv.DictWriter = csv.writer(file)
-                writer.writerow(headers)
+                writer.writerow(self.headers)
         except IOError as e:
             print(f"Error writing to CSV: {e}")
     
-    def add_data(self, data: list):
+    def add_data(self, data_dict: dict[str,]):
+        data = [data_dict.get(header, None) for header in self.headers]
         try:
             with open(self.csv_pathname, mode="a", newline="") as file:
                 writer = csv.writer(file)
