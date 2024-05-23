4cc336bf03fb120eb393e661e8373f93dfc029ed
Zediwan <Jeremy@qualit.ch>
2024-05-23 16:50:14 +0200

    wrote first test for databasemanager

--- tests/database_manager_test.py
+++ tests/database_manager_test.py
@@ -3,4 +3,15 @@ from code.database_manager import DatabaseManager
 
 
 class DatabaseManagerTest(unittest.TestCase):
-    pass
+    def test_csv_creation(self):
+        """
+        This test checks if the csv is created properly
+        """
+        import os
+        
+        headers: list = []
+        headers.append("h1"),
+        headers.append("h2")
+        dbm = DatabaseManager(headers)
+        
+        self.assertTrue(os.path.isfile(dbm.csv_pathname), "CSV was not created")
