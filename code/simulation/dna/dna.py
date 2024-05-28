6b7fe01d09a1d8a93e76f27e1cfc722bcf98683d
Zediwan <Jeremy@qualit.ch>
2024-05-28 10:34:57 +0200

    refactored dna class

--- code/simulation/dna/dna.py
+++ code/simulation/dna/dna.py
@@ -7,6 +7,7 @@ from .gene import Gene
 
 
 class DNA:
+    #region Gene settings
     attack_power_min: float = 0
     attack_power_max: float = 50
     attack_power_mutation_range: float = 2
@@ -38,7 +39,7 @@ class DNA:
     mutation_chance_min: float = 0
     mutation_chance_max: float = 1
     mutation_chance_mutation_range: float = 0.01
-
+    #endregion
     #region class methods
     @classmethod
     def set_attack_power_mutation_range(cls, value):
@@ -84,77 +85,78 @@ class DNA:
         min_reproduction_energy: float,
         reproduction_chance: float
     ) -> None:
+        self.genes: list[Gene] = []
+
         self.color_r_gene: Gene = Gene(
             DNA.color_max,
             DNA.color_min,
             color.r,
             DNA.color_mutation_range,
         )
+        self.genes.append(self.color_r_gene)
         self.color_g_gene: Gene = Gene(
             DNA.color_max,
             DNA.color_min,
             color.g,
             DNA.color_mutation_range,
         )
+        self.genes.append(self.color_g_gene)
         self.color_b_gene: Gene = Gene(
             DNA.color_max,
             DNA.color_min,
             color.b,
             DNA.color_mutation_range,
         )
+        self.genes.append(self.color_b_gene)
         self.attack_power_gene: Gene = Gene(
             DNA.attack_power_max,
             DNA.attack_power_min,
             attack_power,
             DNA.attack_power_mutation_range,
         )
+        self.genes.append(self.attack_power_gene)
         self.prefered_moisture_gene: Gene = Gene(
             DNA.prefered_moisture_max,
             DNA.prefered_moisture_min,
             prefered_moisture,
             DNA.prefered_moisture_muation_range,
         )
+        self.genes.append(self.prefered_moisture_gene)
         self.prefered_height_gene: Gene = Gene(
             DNA.prefered_height_max,
             DNA.prefered_height_min,
             prefered_height,
             DNA.prefered_height_muation_range,
         )
+        self.genes.append(self.prefered_height_gene)
         self.mutation_chance_gene: Gene = Gene(
             DNA.mutation_chance_max,
             DNA.mutation_chance_min,
             muation_chance,
             DNA.mutation_chance_mutation_range
         )
+        self.genes.append(self.mutation_chance_gene)
         self.min_reproduction_health_gene: Gene = Gene(
             DNA.min_reproduction_health_max,
             DNA.min_reproduction_health_min,
             min_reproduction_health,
             DNA.min_reproduction_health_mutation_range
         )
+        self.genes.append(self.min_reproduction_health_gene)
         self.min_reproduction_energy_gene: Gene = Gene(
             DNA.min_reproduction_energy_max,
             DNA.min_reproduction_energy_min,
             min_reproduction_energy,
             DNA.min_reproduction_energy_mutation_range
         )
+        self.genes.append(self.min_reproduction_energy_gene)
         self.reproduction_chance_gene: Gene = Gene(
             DNA.reproduction_chance_max,
             DNA.reproduction_chance_min,
             reproduction_chance,
             DNA.reproduction_chance_mutation_range
         )
-
-        self.genes: list[Gene] = [
-            self.color_r_gene,
-            self.color_g_gene,
-            self.color_b_gene,
-            self.attack_power_gene,
-            self.prefered_moisture_gene,
-            self.prefered_height_gene,
-            self.mutation_chance_gene,
-            self.reproduction_chance_gene
-        ]
+        self.genes.append(self.reproduction_chance_gene)
 
     @property
     def color(self):
