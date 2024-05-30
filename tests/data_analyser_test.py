d4e2fce0ab422091f92c29e52ea36a35cd733b5d
Zediwan <Jeremy@qualit.ch>
2024-05-30 09:41:50 +0200

    commented out old data_analyser code.

---
+++ tests/data_analyser_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from ..src.data_analyser import Data_Analyser
+
+class TestDataAnalyser(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+class TestInit(TestDataAnalyser):
+    def test_init_valid_min_arguments(self):
+        self.width = 100
+        self.height = 100
+        Data_Analyser(self.width, self.height)
